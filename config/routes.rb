a1493bc911e8200ec92858310e647931c88147fa
tomokr <tomo@planna.in>
2021-11-06 22:40:06 -0400

    Add list controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get 'api' => 'api#success'
+  get 'lists/:id' => 'lists#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
